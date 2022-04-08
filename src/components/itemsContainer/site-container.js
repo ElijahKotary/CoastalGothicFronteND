1bdeccc2d5b0d279154e55717ab7818b6e6a6f1e
ElijahKotary <elijah.a.kotary@gmail.com>
2022-04-07 20:47:03 -0400

    heroku path

--- src/components/itemsContainer/site-container.js
+++ src/components/itemsContainer/site-container.js
@@ -12,7 +12,7 @@ export default class Products extends Component {
   }
 
   componentDidMount() {
-    fetch("http://127.0.0.1:5000/products")
+    fetch("https://coastalapi.herokuapp.com/products")
       .then((response) => response.json())
       .then((data) => {
         this.setState({
